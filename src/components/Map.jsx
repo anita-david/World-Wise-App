7327e54de5301f33087cedd47f3dcb591bd684b0
Anita David <anitafunmilayo@gmail.com>
2025-10-09 15:29:40 +0100

    Implemented map interaction

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,16 +1,22 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 
 function Map() {
   const [mapPosition, setMapPosition] = useState([40, 0]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const [searchParams] = useSearchParams();
+  const mapLat = searchParams.get("lat");
+  const mapLng = searchParams.get("lng");
   const navigate = useNavigate();
   const { cities } = useCities();
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },[mapLat, mapLng]
+  );
   return (
     <div
       className={styles.mapContainer}
@@ -20,7 +26,7 @@ function Map() {
     >
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -28,14 +34,22 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
-     { cities.map(city => <Marker position={[city.position.lat, city.position.lng]}>
-        <Popup>
-         <span>{city.emoji}</span> <span>{city.cityName}</span>
-        </Popup>
-      </Marker>)}
+        {cities.map((city) => (
+          <Marker position={[city.position.lat, city.position.lng]}>
+            <Popup>
+              <span>{city.emoji}</span> <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+          
+        ))}
+        <ChangeCenter position={mapPosition}/>
       </MapContainer>
     </div>
   );
 }
-
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
 export default Map;
